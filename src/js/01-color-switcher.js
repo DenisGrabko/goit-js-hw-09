1dee2d435119496ad7bef5bfe544e2bffcc54cfe
Denis <grabko.denis11@gmail.com>
2023-09-01 16:06:26 +0300

    first-task-01-Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let changeBackground;
+//let changeBackgroundColorInterval;
 
+
+
+startBtn.addEventListener("click", () => {
+    if(!startBtn.disable){
+    startBtn.disabled = "true";
+    stopBtn.disabled = "";
+
+    changeBackground = setInterval(()=> {
+        document.body.style.backgroundColor = getRandomHexColor()}, 
+    1000)}
+ });
+   
+
+
+stopBtn.addEventListener("click", () => {
+    //document.body.style.backgroundColor = "";
+    if(!stopBtn.disable) {
+        stopBtn.disabled = "true";
+        startBtn.disabled = "";
+        clearInterval(changeBackground)
+    }
+    
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
